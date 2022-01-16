a0c28b7c617d794c214e296bade7b39b5a374904
Eyup Ucmaz <eyupucmaz@gmail.com>
2022-01-16 11:15:18 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import {CONFIG} from './api/constants'
+
+function App() {
+  console.log(CONFIG)
+  return (
+    <div className="App">
+    </div>
+  )
+}
+
+export default App
